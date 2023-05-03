69e69e82f1151fb9b38ee4ef72e97e91b33d26ea
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-05-03 19:41:59 +0400

    Initial commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  insertProduct(title: string, desc: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, desc, price);
+    this.products.push(newProduct);
+    return prodId;
+  }
+
+  getAllProducts() {
+    return [...this.products];
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    updatedProduct.title = title ? title : updatedProduct.title;
+    updatedProduct.desc = desc ? desc : updatedProduct.desc;
+    updatedProduct.price = price ? price : updatedProduct.price;
+
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(prodId: string) {
+    const index = this.findProduct(prodId)[1];
+    this.products.splice(index, 1);
+    return null;
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Something went wrong');
+    }
+    return [product, productIndex];
+  }
+}
